848c92055159e79997032d6de9c1434c6551c656
kdncode <kdncode@gmail.com>
2018-06-03 22:33:34 -0500

    added Routing (simple method)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,12 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { Routes, RouterModule } from '@angular/router'; // Routing
+
+const routesConfig: Routes = [
+  { path: 'contact', component: ContactComponent },
+  { path: 'detail', component: ContactDetailComponent }
+]
 
 import { AppComponent } from './app.component';
 import { TestComponent } from './test/test.component';
@@ -11,6 +17,8 @@ import { PersonComponent } from './person/person.component';
 import { ListPersonComponent } from './list-person/list-person.component';
 import { ParentComponent } from './parent.component';
 import { ChildComponent } from './child.component';
+import { ContactComponent } from './contact/contact.component';
+import { ContactDetailComponent } from './contact-detail/contact-detail.component';
 
 @NgModule({
   declarations: [
@@ -22,11 +30,14 @@ import { ChildComponent } from './child.component';
     PersonComponent,
     ListPersonComponent,
     ParentComponent,
-    ChildComponent
+    ChildComponent,
+    ContactComponent,
+    ContactDetailComponent
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    RouterModule.forRoot(routesConfig) // Routing
   ],
   providers: [],
   bootstrap: [AppComponent]
