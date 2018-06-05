09c869ef1b2e6c3bd612f8ee8fea5e1ee396109f
kdncode <kdncode@gmail.com>
2018-06-05 02:12:57 -0500

    added template variable + binding types

--- src/app/courses.component.ts
+++ src/app/courses.component.ts
@@ -5,9 +5,17 @@ import { Component } from '@angular/core';
   selector: 'app-courses',
   template: `
     <h1>Hello World + {{ getName() }}</h1>
+    <button class="btn btn-primary" [class.active]="isActive">Save</button>
+    <button [style.backgroundColor]="isActive ? 'green' : 'blue' ">Save</button>
+    <button (click)="onClick()">Click Me </button><br/>
+
+    <input (keyUp.enter)="onKeyUp($event)" > <br/><br/>
+    <!-- Template variable -->
+    <input #email (keyup.enter)="onKeyUp1(email.value)" ><br/><br/>
     <ul *ngFor='let course of courses'>
       <li>{{ course }}</li>
-    </ul>`
+    </ul>
+    <img [src]="github"/>`
 })
 
 export class CoursesComponent {
@@ -15,12 +23,26 @@ export class CoursesComponent {
   name = 'Jizzz Jack';
   age = 1;
   courses;
-
+  github= "https://octodex.github.com/images/okal-eltocat.jpg"
+  isActive = true;
   constructor(coursesService: CoursesService) {
     this.courses = coursesService.getCourses();
   }
 
   getName() {
     return this.name;
+  }
+
+  onClick() {
+    console.log('hello world')
+  }
+
+  onKeyUp($event) {
+    console.log($event.target.value)
+  }
+
+  onKeyUp1(email) {
+    console.log(email);
+
   }
 }
